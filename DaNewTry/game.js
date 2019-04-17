e2c8316d031867c63ceb73d9f75319a43746f215
streq <santirequena@hotmail.com>
2019-04-17 01:22:37 -0300

    lava block logic man I gotta come back to this shit

--- DaNewTry/game.js
+++ DaNewTry/game.js
@@ -15,6 +15,19 @@ var Game = (() => {
         "#00f"
     ];
     
+	class Player {
+		constructor(x,y){
+			this.x = x;
+			this.y = y;
+			
+			this.h = 8;
+			this.w = 8;
+			
+			this.vx = 0;
+			this.vy = 0;
+		}
+	}
+	
 	return {
 		init() {
 			this.world = {
@@ -23,24 +36,30 @@ var Game = (() => {
 				staticGrid: new SingleArrayGrid(32),
 				gridSize: 8
 			};
-			let setGrid = (x, y, val) => {
-				let g = this.world.staticGrid;
-				g.set(x,y,val)
+			
+			//world setup
+			let g = this.world.staticGrid;
+			g.set(0, 5, TYPES.BLOCK);
+			g.set(1, 5, TYPES.BLOCK);
+			g.set(5, 5, TYPES.BLOCK);
+			g.set(6, 5, TYPES.BLOCK);
+			g.set(7, 5, TYPES.BLOCK);
+			g.set(8, 5, TYPES.BLOCK);
+			g.set(9, 5, TYPES.BLOCK);
+			g.set(10, 5, TYPES.BLOCK);
+			g.set(15, 5, TYPES.GOAL);
+			for(var i = 0; i < 32; ++i){
+				g.set(i, 31, TYPES.LAVA);
 			}
-			setGrid(0, 5, TYPES.BLOCK);
-			setGrid(5, 5, TYPES.BLOCK);
-			setGrid(7, 5, TYPES.BLOCK);
-			setGrid(9, 5, TYPES.BLOCK);
-			setGrid(15, 5, TYPES.GOAL);
-			setGrid(10, 9, TYPES.LAVA);
-
-			this.x = 0;
-			this.y = 0;
-			this.vx = 0;
-			this.vy = 0;
+			
+			this.player=new Player(0,0);
+			
 		},
 
 		update(dt, input) {
+			let p = this.player,
+				world = this.world;
+			//input
 			let dir = {
 				x: 0 + (input.R || 0) - (input.L || 0),
 				y: 0 + (input.D || 0) - (input.U || 0)
@@ -49,25 +68,46 @@ var Game = (() => {
 				dir.x *= Math.SQRT1_2;
 				dir.y *= Math.SQRT1_2;
 			}
-			dir.x*=10;
-			dir.y*=10;
+			dir.x*=8;
+			dir.y*=8;
 			
-			this.vx = dir.x;
-			if(dir.y)this.vy = dir.y;
+			//set velocities
+			p.vx = dir.x;
+			if(dir.y)p.vy = dir.y;
 			
-			this.vy += dt*10;
+			p.vy += dt*10;
 			
 			
-			this.x+=this.vx;
-			this.y+=this.vy;
+			//check collision with lava
+			let gs = world.gridSize;
+			world.staticGrid.forEach((tile,x,y)=>{
+				let x1 = x*gs;
+				let y1 = y*gs;
+				let w1 = gs;
+				let h1 = gs;
+				if(tile == TYPES.LAVA){
+					if(Collision.boxBoxMoving(p.x, p.y, p.w, p.h, x1, y1, gs, gs, p.vx, p.vy)){
+						this.die();
+					}
+				}
+			});
+			
+			//move
+			p.x+=p.vx;
+			p.y+=p.vy;
+			
 		},
-
+		
+		die(){
+			this.player = new Player(0,0);
+		},
+		
 		render(ctx) {
-
+			let p = this.player;
 			//render player
 			ctx.clearRect(0, 0, CONSTS.WIDTH, CONSTS.WIDTH);
 			ctx.fillStyle = "#fff";
-			ctx.fillRect(this.x, this.y, 8, 8);
+			ctx.fillRect(p.x, p.y, 8, 8);
 
 			//render grid
 			let grid = this.world.staticGrid;
